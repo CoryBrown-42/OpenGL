2679f55a2201d79855d3f481a1546eba609fa5eb
CoryBrown-42 <yroc.999@gmail.com>
2020-08-17 15:08:45 -0400

    Creating a window

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,3 +1,67 @@
 #include <iostream>
 
-//Test
+#define GLEW_STATIC
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint width = 800, height = 600;
+
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+	GLFWwindow *window = glfwCreateWindow(width, height, "Advanced Programming", nullptr, nullptr);
+
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (nullptr == window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to Initialize GLEW" << std::endl;
+
+		return EXIT_FAILURE;
+	}
+
+	glViewport(0, 0, screenHeight, screenHeight);
+
+	//Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		//0 - 1
+		//RGBA color of the clear to background
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//draw
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
+}
