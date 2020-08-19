f22a8431f9f4e9cd57ee15151481c97b29e882da
CoryBrown-42 <yroc.999@gmail.com>
2020-08-19 13:33:13 -0400

    push for cole

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,31 +1,56 @@
 #include <iostream>
 
+//GLEW
 #define GLEW_STATIC
-
 #include <GL/glew.h>
+
+//GLFW
 #include <GLFW/glfw3.h>
 
+//size of the window
 const GLint width = 800, height = 600;
 
+/*Shader Code*/
+
+//Vertex Shader
+const GLchar* vertexShaderCode =
+"#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Postion = vec4(position.x, position.y,position.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader - Think of this as a pixel shader
+const GLchar* fragmentShaderCode =
+"#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 1.0f, 1.0f, 1.0f);"
+"}\n\0";
 
 int main()
 {
+	//initialize GLFW
 	glfwInit();
 
+	//Set required options for GLFW
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
+	//Create the window
 	GLFWwindow *window = glfwCreateWindow(width, height, "Advanced Programming", nullptr, nullptr);
 
+	//set the frame buffer to the screen size
 	int screenWidth, screenHeight;
 	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
 
+	//Did creating the window fail?
 	if (nullptr == window)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -35,6 +60,7 @@ int main()
 
 	glfwMakeContextCurrent(window);
 
+	// Set this to true so GLEW knows to use a modern function pointers and extensions
 	glewExperimental = GL_TRUE;
 
 	if (GLEW_OK != glewInit())
@@ -45,6 +71,43 @@ int main()
 	}
 
 	glViewport(0, 0, screenHeight, screenHeight);
+
+
+	/*Build and compile our shaders*/
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error - Vertex Shader Compilation Failed\n" << infoLog << std::endl;
+	}
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderCode, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error - Fragment Shader Compilation Failed\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f  //left bottom
+	}
+
 
 	//Game loop
 	while (!glfwWindowShouldClose(window))
